9d514ebe733a10f11c39f9e7f6b60f93e91c2a2a
Jasveen Kaur <60607588+jasveenkaur1299@users.noreply.github.com>
2022-03-17 14:20:09 +0530

    Time: 20 ms (62.13%), Space: 8.9 MB (16.41%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        unordered_map<char,int> mp;
+        for(int i=0;i<magazine.size();i++)
+        {
+            mp[magazine[i]]++;
+        }
+        for(int i=0;i<ransomNote.size();i++)
+        {
+            if(mp[ransomNote[i]]==0 || mp.find(ransomNote[i])==mp.end())
+            {
+                return false;
+            }
+            
+            else
+                mp[ransomNote[i]]--;
+        }
+        return true;
+        
+    }
+};
