7474ee621193fe04568281a7e32e57e7ac169897
Jasveen Kaur <60607588+jasveenkaur1299@users.noreply.github.com>
2022-06-19 22:32:34 +0530

    Time: 73 ms (42.75%), Space: 17.2 MB (12.26%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,58 +8,22 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-    ListNode* temp1=headA;
-    ListNode* temp2=headB;
-    ListNode* temp3=headB;
-        int count1=0;
-        int count2=0;
-        if(!headA||!headB)
-            return NULL;
-        while(temp1!=NULL)
-        {
-            count1++;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        unordered_set<ListNode*> set;
+        ListNode* temp1=headA;
+        ListNode* temp2=headB;
+        while(temp1!=NULL){
+            set.insert(temp1);
             temp1=temp1->next;
         }
         while(temp2!=NULL)
         {
-            count2++;
+            if(set.find(temp2)!=set.end())
+                return temp2;
             temp2=temp2->next;
         }
-        temp1=headA;
-        temp2=headB;
-        if(count2>count1){
-        while(temp1!=NULL){
-            temp3=temp2;
-            while(temp3!=NULL)
-            {
-                if(temp1==temp3)
-                    return temp1;
-                else
-                {
-                    //cout<<temp1->val<< " "<<temp3->val <<" not match"<<endl;
-                    temp3=temp3->next;
-                }
-            }
-            temp1=temp1->next; 
-            temp2=temp2->next;
-        }}
-        else{
-        while(temp2!=NULL){
-            temp3=temp1;
-            while(temp3!=NULL)
-            {
-                if(temp2==temp3)
-                    return temp2;
-                else
-                {
-                    //cout<<temp1->val<< " "<<temp3->val <<" not match"<<endl;
-                    temp3=temp3->next;
-                }
-            }
-            temp1=temp1->next; 
-            temp2=temp2->next;
-        }}
-        return NULL;   
+        return NULL;
+        
     }
 };
